9f61b11c13d72e828d2d13652463418f0ad216ca
6ocra3 <6o3kmm@mail.ru>
2023-06-23 16:11:45 +0300

    add different square display

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { ArrowRight } from 'react-feather'
 import './App.css'
+
 // #343d46,#4f5b66,#65737e,#a7adba,#c0c5ce
 
 function App() {
@@ -11,10 +13,16 @@ function App() {
         <div className="task__days">
           {days.map((v) => {
             if (v == 0) {
-              return <div key={key} className="task__day"></div>
+              return <div key={key} className="task__day none"></div>
+            }
+            else if (v == 1) {
+              return <div key={key} className="task__day empty"></div>
+            }
+            else if (v == 2) {
+              return <div key={key} className="task__day fill"></div>
             }
             else {
-              return <></>
+              return <div key={key} className="task__day arrow"><ArrowRight className='icon' size={18} /></div>
             }
           })}
         </div>
@@ -22,7 +30,7 @@ function App() {
       </section>
     )
   }
-  const tasks = [{ key: 0, task: "Hello world!", column: 1, days: [0, 0, 0, 0, 0, 0, 0] }]
+  const tasks = [{ key: 0, task: "Hello world!", column: 1, days: [1, 2, 3, 0, 0, 0, 0] }]
 
   const head = [{ key: 10, value: "Пн" }, { key: 11, value: "Вт" }, { key: 12, value: "Ср" }, { key: 13, value: "Чт" }, { key: 14, value: "Пт" }, { key: 15, value: "Сб" }, { key: 16, value: "Вс" }]
   const yach = [{ key: 1, value: 0 }, { key: 2, value: 0 }, { key: 3, value: 0 }, { key: 4, value: 0 }, { key: 5, value: 0 }, { key: 6, value: 0 }, { key: 7, value: 0 }]
@@ -48,7 +56,9 @@ function App() {
           <ul className="list__ul">
             {tasks.map(t => {
               if (t.column == 1) {
-                return (<li className="list__point">{t.task}</li>)
+                return (<li className="list__point">
+                  <div className="list__point_square"></div>
+                  {t.task}</li>)
               }
             })}
             <li className="list__add">Добавить задачу</li>
