6c14e05462fd22a8ab018ff8e69a6a1d90a891ae
6ocra3 <6o3kmm@mail.ru>
2023-07-08 20:44:34 +0300

    Redux defaultState from backend

--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,19 @@ import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 const a = [1, 0]
 
-const defaultState = {
-  tasks: {
-    0: { task: "Hello world!", status: 0, days: [0, 0, 0, 0, 0, 0, 0] },
-    1: { task: "Hello world 2!", status: 1, days: [0, 0, 0, 0, 0, 0, 0] },
-    2: { task: "Hello world 3!", status: 2, days: [0, 0, 0, 0, 0, 0, 0] }
-  },
-  tr_order: JSON.parse(JSON.stringify(a)),
-  col_order: [[0, 1], [2], []]
+var response = await fetch('http://127.0.0.1:5005/get_week_tasks/2022-07-03');
+const tasks = await response.json();
+var response = await fetch('http://127.0.0.1:5005/get_week/2022-07-03');
+const week = await response.json();
+const defaultState = tasks && week && {
+  tasks: tasks,
+  tr_order: week.tracker_order,
+  list_order: week.list_order
 }
 
 const reducer = (state = defaultState, action) => {
   const temp_tasks = JSON.parse(JSON.stringify(state.tasks))
-  const temp_col_order = JSON.parse(JSON.stringify(state.col_order))
+  const temp_col_order = JSON.parse(JSON.stringify(state.list_order))
   switch (action.type) {
     case "change_display":
       temp_tasks[action.payload.task].days[action.payload.day] = (temp_tasks[action.payload.task].days[action.payload.day] + 1) % 4
@@ -32,7 +32,7 @@ const reducer = (state = defaultState, action) => {
       const new_task = { task: action.payload.task, status: 0, days: [0, 0, 0, 0, 0, 0, 0] }
       temp_tasks[maxKey + 1] = JSON.parse(JSON.stringify(new_task))
       temp_col_order[action.payload.col].push(maxKey + 1)
-      return { ...state, col_order: temp_col_order, tasks: temp_tasks }
+      return { ...state, list_order: temp_col_order, tasks: temp_tasks }
     case "add_to_tr":
       console.log(!(action.payload.task in state.tr_order))
       console.log(state.tr_order, action.payload.task)
@@ -47,13 +47,13 @@ const reducer = (state = defaultState, action) => {
       return state
   }
 }
+const store = configureStore({ reducer: reducer, preloadedState: defaultState })
 
-const store = configureStore({ reducer: reducer })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    {defaultState && <Provider store={store}>
       <App />
-    </Provider>
+    </Provider>}
   </React.StrictMode>,
 )
