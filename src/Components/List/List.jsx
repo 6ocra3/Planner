c99cc051863d9a82954e574338b9d45a5cfd6572
6ocra3 <6o3kmm@mail.ru>
2023-07-12 23:10:56 +0300

    demo d&d

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -7,7 +7,7 @@ import { Check } from 'react-feather';
 import { FiX } from "react-icons/fi";
 import { FiCheck } from "react-icons/fi";
 
-function List() {
+function List({ dragTask, setDragTask }) {
     const dispatch = useDispatch()
     const [inp, setInp] = useState(-1)
     const tasks = useSelector(state => state.tasks)
@@ -22,6 +22,17 @@ function List() {
         setInp(-1)
     }
 
+    function startHandler(e, key) {
+    }
+    function dragEndHandler(e) {
+    }
+    function dragOverHandler(e) {
+        e.preventDefault();
+    }
+    function dropHandler(e) {
+        e.preventDefault();
+    }
+
     return (
         <div className="list">
             <h1 className="list__header">Сделать за неделю</h1>
@@ -29,12 +40,17 @@ function List() {
                 {tasks && list_order && list_order.map((v, index) => {
                     return (
                         <ul key={shortid.generate()} className='list__ul'>
-                            {v.map((el) => {
-                                return (<li key={el} className="list__point">
-                                    <div className="list__point_square" onClick={() => { dispatch({ type: "change_status", payload: { task: el } }) }}>{icons[tasks[el].status]} </div>
-                                    <p className={tasks[el].status != 0 ? "task_finished" : ""} onClick={() => { dispatch({ type: "add_to_tr", payload: { task: el } }) }}
+                            {v.map((key) => {
+                                return (<li key={key} className="list__point">
+                                    <div className="list__point_square" onClick={() => { dispatch({ type: "change_status", payload: { task: key } }) }}>{icons[tasks[key].status]} </div>
+                                    <p className={tasks[key].status != 0 ? "task_finished" : ""} onClick={() => { dispatch({ type: "add_to_tr", payload: { task: key } }) }}
+                                        onDragStart={(e) => startHandler(e, key)}
+                                        onDragLeave={(e) => dragEndHandler(e)}
+                                        onDragEnd={(e) => dragEndHandler(e)}
+                                        onDragOver={(e) => dragOverHandler(e)}
+                                        onDrop={(e) => dropHandler(e, key)}
                                         draggable={true}
-                                    >{tasks[el].task.length > 20 ? tasks[el].task.slice(0, 18) + "..." : tasks[el].task}</p></li>)
+                                    >{tasks[key].task.length > 20 ? tasks[key].task.slice(0, 18) + "..." : tasks[key].task}</p></li>)
                             }
                             )}
                             {inp == index ? <li className="list__point">
