7351e76f317b3debeb507672d5a4f755395d9a8c
6ocra3 <6o3kmm@mail.ru>
2023-08-07 16:23:45 +0300

    fetch for change list order

--- src/main.jsx
+++ src/main.jsx
@@ -104,6 +104,16 @@ const reducer = (state = defaultState, action) => {
       const new_tracker_order = JSON.parse(JSON.stringify(action.payload.new_tracker_order))
       return { ...state, tracker_order: new_tracker_order }
     case "change_list_order":
+      fetch('http://127.0.0.1:5005/edit_week', {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          date: state.mon_date.slice(0, 10),
+          list_order: action.payload.new_list_order
+        }),
+      })
       const new_list_order = JSON.parse(JSON.stringify(action.payload.new_list_order))
       return { ...state, list_order: new_list_order }
     case "add_to_tr":
