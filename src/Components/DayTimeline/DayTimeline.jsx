7d8e79c9ab4c63d497a5744447df31b07727dada
6ocra3 <6o3kmm@mail.ru>
2023-08-27 15:05:04 +0300

    add dt__buttons block

--- src/Components/DayTimeline/DayTimeline.jsx
+++ src/Components/DayTimeline/DayTimeline.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import "./DayTimeline.css";
+import { FiMinus, FiPlus } from "react-icons/fi";
 export default function DayTimeline() {
   const [hours, setHours] = useState([9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]);
   return (
     <div className="dt">
+      <div className="dt__buttons">
+        <FiPlus className="dt__button" />
+        <FiMinus className="dt__button" />
+      </div>
       <div className="dt__hours">
         {hours.map((h) => {
           return (
@@ -12,6 +17,10 @@ export default function DayTimeline() {
             </p>
           );
         })}
+      </div>
+      <div className="dt__buttons">
+        <FiPlus className="dt__button" />
+        <FiMinus className="dt__button" />
       </div>
     </div>
   );
